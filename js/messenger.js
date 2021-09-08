c42637cea7e92322463606dff7cbbf3cf80a788b
blackice18 <sdsdfhshs.dima@yandex.ru>
2021-09-08 17:29:53 +0300

    messenger hover

--- js/messenger.js
+++ js/messenger.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.messenger__list-item').on('mouseenter',function () {
         $(this).children('.additionalThreePoints').removeClass('hide');
-    }, function () {
+    }).on('mouseleave', function () {
         $(this).children('.additionalThreePoints').addClass('hide');
     });
 });
